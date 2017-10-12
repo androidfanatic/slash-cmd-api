b2be6eda29ddf804d76ccc4445c6640a2ad61e94
Android Fanatic <manish_raj@outlook.com>
2017-10-12 22:04:24 +0530

    send hi back

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/hello', function(request, response) {
+  response.send('hello from api')
 })
 
 app.listen(app.get('port'), function() {
